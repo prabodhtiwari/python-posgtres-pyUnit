f4a61392c8e4c4d92f2e1a9cd4b62c707edee4db
Prabodh <prabodhtiwari@outlook.com>
2018-05-23 17:37:07 +0530

    environment variable support added

--- db_client.py
+++ db_client.py
@@ -1,9 +1,6 @@
-import configparser
+import os
 import psycopg2
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
 def connect():
-    return psycopg2.connect(database=config['postgresDB']['database'], user=config['postgresDB']['user'], password=config['postgresDB']['password'], host=config['postgresDB']['host'], port=config['postgresDB']['port'])
+    return psycopg2.connect(database=os.environ["DB"], user=os.environ["USER"], password=os.environ["PASSWORD"], host=os.environ["HOST"], port=os.environ["PORT"])
 
