c41e143090ee3bc131be8132667f5bbe294c1ed4
Prabodh <prabodhtiwari@outlook.com>
2018-05-30 22:11:33 +0530

    minor

--- postgres-setup-test.py
+++ postgres-setup-test.py
@@ -23,11 +23,13 @@ def second_test():
 
 
 class MyTestCase(unittest.TestCase):
-    def test_postgres_setup(self):
+    def test_postgres_first_setup(self):
         first = first_test()
-        second = second_test()
         self.assertEqual(first, [(1, 'one', 'Test Store One',
                                   'A', 'USD'), (2, 'two', 'Test Store Two', 'A', None)])
+
+    def test_postgres_second_setup(self):
+        second = second_test()
         self.assertEqual(second, [(2, datetime.date(2018, 1, 1), 2018, 1, 1, 1), (2, datetime.date(2018, 1, 2), 2018, 1, 2, 2), (3, datetime.date(2018, 1, 3), 2018, 1, 3, 3), (4, datetime.date(
             2018, 1, 4), 2018, 1, 4, 4), (5, datetime.date(2018, 1, 5), 2018, 1, 5, 5), (6, datetime.date(2018, 1, 6), 2018, 1, 6, 6), (7, datetime.date(2018, 1, 7), 2018, 1, 7, 7)])
 
