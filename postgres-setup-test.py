59bd2f02c3e355c14871b5303e28f8d557be298b
Prabodh <prabodhtiwari@outlook.com>
2018-05-30 22:02:13 +0530

    postgres_test_setup added

--- postgres-setup-test.py
+++ postgres-setup-test.py
@@ -1,8 +1,19 @@
 import unittest
 import  fetch
 
+import db_client
+
+def test():
+    conn = db_client.connect()
+    cur = conn.cursor()
+
+    cur.execute("select * from dim_store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
 class MyTestCase(unittest.TestCase):
-    def test_default_greeting_set(self):
+    def test_postgres_setup(self):
         greeter = fetch.test()
         self.assertEqual(greeter, [(1, 'one', 'Test Store One', 'A', 'USD'), (2, 'two', 'Test Store Two', 'A', None)])
 
