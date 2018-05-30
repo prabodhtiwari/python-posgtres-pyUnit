a779a033032f7a568666f09f3e387ffb22ccdb76
Prabodh <prabodhtiwari@outlook.com>
2018-05-31 00:56:53 +0530

    minor

--- export_hive_by_hive_sql.py
+++ export_hive_by_hive_sql.py
@@ -4,9 +4,6 @@ import unittest
 import os
 
 def first_test():
-    d = dict(os.environ)
-    print os.environ["EDHDIST"]
-    print d["EDHDIST"]
     return subprocess.check_output(["hive", "-S", "-f", os.environ["EDHDIST"]+"/scripts/hql/dim_store_extract.sql"], env = d)
 
 print first_test()
@@ -14,7 +11,7 @@ print first_test()
 class MyTestCase(unittest.TestCase):
     def test_pig_first_setup(self):
         first = first_test()
-        self.assertEqual(json.dumps(first), '[[1, "one", "Test Store One", "A", "USD"], [2, "two", "Test Store Two", "A", null]]')
+        self.assertEqual(json.dumps(first), '"1\\tone\\tTest Store One\\tA\\tUSD\\n2\\ttwo\\tTest Store Two\\tA\\tNULL\\n"')
 
 if __name__ == '__main__':
     unittest.main()
