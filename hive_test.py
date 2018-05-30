7c8007ac321c89083ab50f8db6ddf10e8f43b212
Prabodh <prabodhtiwari@outlook.com>
2018-05-30 23:21:58 +0530

    minor

--- hive_test.py
+++ hive_test.py
@@ -5,13 +5,13 @@ import json
 conn = hive.Connection(host="0.0.0.0", port=10000, username="hive")
 cursor = conn.cursor()
 
-def first_test():
-    cursor.execute("show databases")
-    for result in cursor.fetchall():
-        # print(json.dumps(result))
-        return result
+# def first_test():
+cursor.execute("show databases")
+for result in cursor.fetchall():
+# print(json.dumps(result))
+ print result
 
-print first_test()
+# print first_test()
 
 # class MyTestCase(unittest.TestCase):
 #     def test_hive_first_setup(self):
