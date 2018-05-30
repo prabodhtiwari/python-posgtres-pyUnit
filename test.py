301f66aa4b39483ff44f9738b107646b65e9f10f
Prabodh <prabodhtiwari@outlook.com>
2018-05-30 21:57:45 +0530

    test case added

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import  fetch
 class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
         greeter = fetch.test()
-        self.assertEqual(greeter, 'Hello world!')
+        self.assertEqual(greeter, [(1, 'one', 'Test Store One', 'A', 'USD'), (2, 'two', 'Test Store Two', 'A', None)])
 
 if __name__ == '__main__':
     unittest.main()
