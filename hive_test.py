693ef1274a10ae3499dd9eff6c2edc8f90f7ae2a
Prabodh <prabodhtiwari@outlook.com>
2018-05-30 23:07:28 +0530

    minor

--- hive_test.py
+++ hive_test.py
@@ -1,7 +1,21 @@
 from pyhive import hive
+import unittest
 
-conn = hive.Connection(host="localhost", port=10000, username="hive")
+conn = hive.Connection(host="0.0.0.0", port=10000, username="hive")
 cursor = conn.cursor()
-cursor.execute("show databases")
-for result in cursor.fetchall():
-  print(result)
+
+def first_test():
+    cursor.execute("show databases")
+    for result in cursor.fetchall():
+        print(result)
+        return result
+
+class MyTestCase(unittest.TestCase):
+    def test_hive_first_setup(self):
+        first = first_test()
+        self.assertEqual(first, (u'default',)
+(u'dev_gglcloud_edh',))
+
+
+if __name__ == '__main__':
+    unittest.main()
