6604d497bd09c57339732ae8d1effe059ed46f02
Prabodh <prabodhtiwari@outlook.com>
2018-05-30 22:03:37 +0530

    minor

--- fetch.py
+++ fetch.py
@@ -4,7 +4,7 @@ def test():
     conn = db_client.connect()
     cur = conn.cursor()
 
-    cur.execute("select * from dim_store")
+    cur.execute("select * from dim_time_date")
     rows = cur.fetchall()
     res = ""
     for row in rows:
