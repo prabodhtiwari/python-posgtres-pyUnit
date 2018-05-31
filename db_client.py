63c642aca570908f90cb2c0750d7056c30da4337
Prabodh <prabodhtiwari@outlook.com>
2018-06-01 00:42:52 +0530

    minor

--- db_client.py
+++
@@ -1,6 +0,0 @@
-import os
-import psycopg2
-
-def connect():
-    return psycopg2.connect(database=os.environ["DB"], user=os.environ["USER"], password=os.environ["PASSWORD"], host=os.environ["HOST"], port=os.environ["PORT"])
-
